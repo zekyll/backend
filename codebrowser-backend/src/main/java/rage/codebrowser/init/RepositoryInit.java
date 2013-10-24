e41c4d02a8ea5283fb5d586a5ec7bb9fdb30ed64
rikniitt <riku.niittymaki@gmail.com>
2013-10-24 20:05:01 +0300

    TestRepositoryInitService

---
+++ codebrowser-backend/src/main/java/rage/codebrowser/init/RepositoryInit.java
@@ -0,0 +1,21 @@
+
+package rage.codebrowser.init;
+
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class RepositoryInit {
+
+    @Autowired
+    RepositoryInitService service;
+
+    @PostConstruct
+    @Transactional
+    public void init() {
+        service.initRepository();
+    }
+
+}
---
+++ codebrowser-backend/src/main/java/rage/codebrowser/init/TestRepositoryInitService.java
@@ -0,0 +1,77 @@
+package rage.codebrowser.init;
+
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import rage.codebrowser.dto.Course;
+import rage.codebrowser.dto.Student;
+import rage.codebrowser.repository.CourseRepository;
+import rage.codebrowser.repository.ExerciseAnswerRepository;
+import rage.codebrowser.repository.ExerciseRepository;
+import rage.codebrowser.repository.SnapshotFileRepository;
+import rage.codebrowser.repository.SnapshotRepository;
+import rage.codebrowser.repository.StudentRepository;
+import rage.codebrowser.repository.TestRepository;
+
+
+
+
+public class TestRepositoryInitService implements RepositoryInitService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+    @Autowired
+    private CourseRepository courseRepository;
+    @Autowired
+    private ExerciseRepository exerciseRepository;
+    @Autowired
+    private SnapshotRepository snapshotRepository;
+    @Autowired
+    private SnapshotFileRepository snapshotFileRepository;
+    @Autowired
+    private ExerciseAnswerRepository exerciseAnswerRepository;
+    @Autowired
+    private TestRepository testRepository;
+
+
+    @Override
+    public void initRepository() {
+        seedTestData();
+        System.out.println("**************** TEST REPOSITORY INIT DONE");
+    }
+
+    private void seedTestData() {
+        createCourses();
+        createStudents();
+    }
+
+    private void createCourses() {
+        String[] courses = new String[]{"unit-test-course", "integration-test-course"};
+        for (String name : courses) {
+            createCourse(name);
+        }
+    }
+
+    private void createCourse(String name) {
+        Course course = new Course();
+        course.setName(name);
+        courseRepository.save(course);
+    }
+
+    private void createStudents() {
+        int courseIndex = 1;
+        for( Course c : courseRepository.findAll() ) {
+            for (int i = 1; i <= courseIndex * 10 ; i++) {
+                Student s = new Student();
+                s.setName(c.getName() + "_student_" + String.format("%04d", i));
+
+                s = studentRepository.save(s);
+
+                c.getStudents().add(s);
+                c = courseRepository.save(c);
+            }
+            courseIndex++;
+        }
+    }
+
+
+}
