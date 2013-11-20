d0afd23605d1e6f340a77ccdf12a603d5aa7b558
Krista <kristalongi@gmail.com>
2013-11-20 23:21:10 +0200

    fixed bug in adding same tagname to category twice

--- codebrowser-backend/src/main/java/rage/codebrowser/controller/TagCategoryRESTController.java
+++ codebrowser-backend/src/main/java/rage/codebrowser/controller/TagCategoryRESTController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -89,13 +90,15 @@ public class TagCategoryRESTController {
     @RequestMapping(value = {"tagcategories/{tagCategoryId}"}, method = RequestMethod.PUT, consumes = "application/json", produces = "application/json")
     @ResponseBody
     public TagCategory postTagCategoryTagNames(@RequestBody TagCategory tagcategory) {
+        TagCategory existing = tagCategoryRepository.findByName(tagcategory.getName());
         for (TagName tagname : tagcategory.getTagnames()) {
             if (tagname.addTagCategory(tagcategory)) {
                 tagNameRepository.save(tagname);
+                existing.addTagName(tagname);
             }
         }
-        tagCategoryRepository.save(tagcategory);
-        return tagcategory;
+        tagCategoryRepository.saveAndFlush(existing);
+        return existing;
     }
         
     @RequestMapping(value = {"tagcategories"}, method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
