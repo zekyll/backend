3ec44ebba60684c6ce2b6323812dc9156983dd9f
Timo <timjlah@gmail.com>
2013-12-05 16:50:11 +0200

    Extended comment search to include exercise, course, student & user name.

--- codebrowser-backend/src/main/java/rage/codebrowser/repository/CommentRepository.java
+++ codebrowser-backend/src/main/java/rage/codebrowser/repository/CommentRepository.java
@@ -16,6 +16,6 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query("SELECT u from Comment u where (u.student = ?1 and u.course = ?2 and u.exercise = ?3 and u.snapshot = ?4) or (u.student = ?1 and u.course = ?2 and u.exercise = ?3 and u.snapshot = NULL)")
     Page<Comment> findByStudentAndCourseAndExerciseAndSnapshotOrSnapshotIsNull(Student student, Course course, Exercise exercise, Snapshot snapshot, Pageable pageable);
 
-    @Query("SELECT c FROM Comment c WHERE LOWER(c.comment) LIKE %:searchString%")
+    @Query("SELECT c FROM Comment c WHERE LOWER(c.comment) LIKE %:searchString% OR LOWER(c.exercise.name) LIKE %:searchString% OR LOWER(c.course.name) LIKE %:searchString% OR LOWER(c.student.name) LIKE %:searchString% OR LOWER(c.username) LIKE %:searchString%")
     Page<Comment> findByCommentContaining(@Param("searchString") String searchString, Pageable pageable);
 }
